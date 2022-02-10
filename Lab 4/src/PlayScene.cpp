d6d832055b910160ff767e07bf043a78e1e3cdb7
Corundrum <Simon.Sereda@georgebrown.ca>
2022-02-10 12:26:01 -0500

    cleaned up the playscene and modified the ship class

---
+++ Lab 4/src/PlayScene.cpp
@@ -0,0 +1,117 @@
+#include "PlayScene.h"
+#include "Game.h"
+#include "EventManager.h"
+
+// required for IMGUI
+#include "imgui.h"
+#include "imgui_sdl.h"
+#include "Renderer.h"
+#include "Util.h"
+
+PlayScene::PlayScene()
+{
+	PlayScene::start();
+}
+
+PlayScene::~PlayScene()
+= default;
+
+void PlayScene::draw()
+{
+	drawDisplayList();
+	
+	SDL_SetRenderDrawColor(Renderer::Instance().getRenderer(), 255, 255, 255, 255);
+}
+
+void PlayScene::update()
+{
+	updateDisplayList();
+
+
+}
+
+void PlayScene::clean()
+{
+	removeAllChildren();
+}
+
+void PlayScene::handleEvents()
+{
+	EventManager::Instance().update();
+
+	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_ESCAPE))
+	{
+		TheGame::Instance().quit();
+	}
+
+	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_1))
+	{
+		TheGame::Instance().changeSceneState(START_SCENE);
+	}
+
+	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_2))
+	{
+		TheGame::Instance().changeSceneState(END_SCENE);
+	}
+}
+
+void PlayScene::start()
+{
+	// Set GUI Title
+	m_guiTitle = "Play Scene";
+	m_bDebugView = false;
+
+	m_pTarget = new Target();
+	addChild(m_pTarget);
+
+	m_pSpaceShip = new SpaceShip();
+	addChild(m_pSpaceShip);
+
+	SoundManager::Instance().load("../Assets/audio/yay.ogg", "yay", SOUND_SFX);
+	SoundManager::Instance().load("../Assets/audio/thunder.ogg", "boom", SOUND_SFX);
+
+	ImGuiWindowFrame::Instance().setGUIFunction(std::bind(&PlayScene::GUI_Function, this));
+}
+
+void PlayScene::GUI_Function()
+{
+	// Always open with a NewFrame
+	ImGui::NewFrame();
+
+	// See examples by uncommenting the following - also look at imgui_demo.cpp in the IMGUI filter
+	//ImGui::ShowDemoWindow();
+	
+	ImGui::Begin("Lab  Debug Properties", NULL, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoMove);
+
+	ImGui::Separator();
+
+	static bool toggleSeek = m_pSpaceShip->isEnabled();
+	if (ImGui::Checkbox("Toggle Seek", &toggleSeek))
+	{
+		m_pSpaceShip->setEnabled(toggleSeek);
+	}
+
+	static bool toggleDebug = false;
+	if (ImGui::Checkbox("Toggle Debug", &toggleDebug))
+	{
+		m_bDebugView = toggleDebug;
+	}
+	
+	ImGui::Separator();
+
+	// target properties
+	
+	static float target_position[2] = { m_pTarget->getTransform()->position.x, m_pTarget->getTransform()->position.y};
+	if(ImGui::SliderFloat2("Target Position", target_position, 0.0f, 800.0f))
+	{
+		m_pTarget->getTransform()->position = glm::vec2(target_position[0], target_position[1]);
+	}
+
+	static float ship_position[2] = { m_pSpaceShip->getTransform()->position.x, m_pSpaceShip->getTransform()->position.y };
+	if (ImGui::SliderFloat2("Ship Position", ship_position, 0.0f, 800.0f))
+	{
+		m_pSpaceShip->getTransform()->position = glm::vec2(ship_position[0], ship_position[1]);
+	}
+
+	ImGui::End();
+}
