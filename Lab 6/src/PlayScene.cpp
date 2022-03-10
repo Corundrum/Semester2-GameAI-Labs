5a210a3b4f313251c68d17f2ed99d34896b14586
Corundrum <Simon.Sereda@georgebrown.ca>
2022-03-10 13:39:34 -0500

    completed check ship LOS function

--- Lab 6/src/PlayScene.cpp
+++ Lab 6/src/PlayScene.cpp
@@ -148,6 +148,29 @@ void PlayScene::m_toggleGrid(const bool state)
 
 void PlayScene::m_checkShipLOS(DisplayObject* target_object)
 {
+	m_pSpaceShip->setHasLOS(false);
+
+	auto ShipToTargetDistance = Util::getClosestEdge(m_pSpaceShip->getTransform()->position, target_object);
+	if (ShipToTargetDistance <= m_pSpaceShip->getLOSDistance())
+	{
+		std::vector<DisplayObject*> contactList;
+		for (auto object : getDisplayList())
+		{
+			if (object->getType() == PATH_NODE) { continue; } // ignore path nodes
+			if (object->getType() != m_pSpaceShip->getType() && object->getType() != target_object->getType())
+			{
+				//check if object is closer to spaceship than target
+				auto ShipToObjectDistance = Util::getClosestEdge(m_pSpaceShip->getTransform()->position, object);
+				if (ShipToObjectDistance <= ShipToTargetDistance)
+				{
+					contactList.push_back(object);
+				}
+			}
+		}
+		auto hasLOS = CollisionManager::LOSCheck(m_pSpaceShip, m_pSpaceShip->getTransform()->position + m_pSpaceShip->getCurrentDirection() * m_pSpaceShip->getLOSDistance(), contactList, target_object);
+		m_pSpaceShip->setHasLOS(hasLOS);
+	}
+
 }
 
 void PlayScene::m_storeObstacles()
